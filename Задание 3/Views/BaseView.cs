1404068fba3e42ae53e8f388f1ceb165830edab9
Alexander <Coltranexpro@yandex.ru>
2022-02-08 20:57:55 +0300

    Added GetUserResponse for BaseView

--- Задание 3/Views/BaseView.cs
+++ Задание 3/Views/BaseView.cs
@@ -7,8 +7,35 @@ using System.Threading.Tasks;
 
 namespace DiaryProject.Views
 {
-    public class BaseView : IBaseView
+    public abstract class BaseView : IBaseView
     {
+        private const string RepeatText = "Вы ввели пустое значение, повторите ввод!";
 
+
+        public void ShowMessage(string messageText)
+        {
+
+        }
+
+        public string GetUserResponse()
+        {
+            bool isGetResponse;
+            string response;
+
+            do
+            {
+                response = Console.ReadLine();
+
+                if (String.IsNullOrEmpty(response))
+                {
+                    Console.WriteLine(RepeatText);
+                    break;
+                }
+
+                isGetResponse = true;
+            } while (isGetResponse);
+
+            return response;
+        }
     }
 }
