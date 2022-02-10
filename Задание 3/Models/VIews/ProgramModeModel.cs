c9d41479af08c8bbdd583c3a3fbfc6f3ec442c8e
Alexander <Coltranexpro@yandex.ru>
2022-02-10 19:56:49 +0300

    Added ProgramModeModel

---
+++ Задание 3/Models/VIews/ProgramModeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiaryPetProject.Privitives.Enums;
+
+namespace DiaryPetProject.Models.VIews
+{
+    public class ProgramModeModel
+    {
+        public MainMenuMode Mode { get; set; }
+        public string MessageText { get; set; }
+    }
+}
