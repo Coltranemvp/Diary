4c402fe2ce7dcd16312ce5c7364523aafa1b6390
Alexander <Coltranexpro@yandex.ru>
2022-02-10 18:23:35 +0300

    Minnir fix

---
+++ Задание 3/Primitives/Enum/ProgramMode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_3.Privitives.Enums
+{
+    public enum ProgramMode
+    {
+        Menu,
+        AddEvent,
+        DeleteEvent,
+        AddPushForEvent,
+        WatchEvents,
+        ExportEvent
+    }
+}
