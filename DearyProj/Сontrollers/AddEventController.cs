f983d898f768d2869d9b0ae955c09c716d112079
Alexander <76205020+Coltranemvp@users.noreply.github.com>
2022-03-20 12:16:24 +0300

    Added AddEventController

---
+++ DearyProj/Сontrollers/AddEventController.cs
@@ -0,0 +1,42 @@
+﻿using DearyPetProj.Сontrollers.Interfaces;
+using DearyPetProj.Сontrollers.Navigation.Primitives;
+using DearyPetProj.Сontrollers.Navigation.Primitives.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DearyPetProj.Сontrollers
+{
+    public class AddEventController : ControllerResult<Result>
+
+    {
+        public override Result GetResult()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetNavigationWay()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Start()
+        {
+            Console.WriteLine("AddEventController");
+            Console.ReadLine();
+        }
+       
+    }
+
+    public class Result : BaseResult
+    {
+        public Result(string message)
+        {
+            Message = message;
+        }
+
+        public string Message { get; set; }
+    }
+}
