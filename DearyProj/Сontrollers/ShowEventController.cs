9e651b318bf91f016d4d05ee8792672712ac00be
Alexander <76205020+Coltranemvp@users.noreply.github.com>
2022-03-20 12:16:34 +0300

    added ShowEventController

---
+++ DearyProj/Сontrollers/ShowEventController.cs
@@ -0,0 +1,30 @@
+﻿using DearyPetProj.Сontrollers.Navigation.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DearyPetProj.Сontrollers
+{
+    public class ShowEventController<T> : ControllerParam<T>
+        where T : BaseParam
+    {
+        public override void PrepareParam(T param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetNavigationWay()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
