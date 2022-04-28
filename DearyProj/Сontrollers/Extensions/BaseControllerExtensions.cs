8896352e972b8712029b4e6d4140facead8ff203
Alexander <76205020+Coltranemvp@users.noreply.github.com>
2022-04-28 16:26:08 +0300

    Added Active state for controllers

--- DearyProj/Сontrollers/Extensions/BaseControllerExtensions.cs
+++ DearyProj/Сontrollers/Extensions/BaseControllerExtensions.cs
@@ -1,10 +1,5 @@
 ﻿using DearyPetProj.Сontrollers.Interfaces;
 using DearyPetProj.Сontrollers.Navigation.Primitives.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DearyPetProj.Сontrollers.Extensions
 {
@@ -13,29 +8,50 @@ namespace DearyPetProj.Сontrollers.Extensions
 
         public static void NavigateTo(this BaseController baseController, IControllerWithoutParamResult newController)
         {
+            baseController.Active = false;
+
             newController.Start();
+
+            baseController.Active = true;
         }
 
         public static void NavigateTo<TParam>(this BaseController baseController, IControllerWithParam<TParam> newController, TParam param)
             where TParam : IParam
         {
+            baseController.Active = false;
+
             newController.PrepareParam(param);
             newController.Start();
+
+            baseController.Active = true;
         }
 
         public static T NavigateTo<T>(this BaseController baseController, IControllerWithResult<T> newController)
             where T : IResult
         {
-            newController.Start();
+            baseController.Active = false;           
+
+            newController.Start();            
+
+            baseController.Active = true;
+
             return newController.GetResult();
+
+
         }
 
-        public static IResult NavigateTo<TParam, TResult>(this BaseController baseController, IControllerWithParamResult<TParam, TResult> newController, TParam param)
+        public static IResult  NavigateTo<TParam, TResult> (this BaseController baseController,
+            IControllerWithParamResult<TParam, TResult> newController, TParam param)
             where TParam : IParam
             where TResult : IResult
         {
+            baseController.Active = false;
+
             newController.PrepareParam(param);
-            newController.Start();
+            newController.Start();            
+
+            baseController.Active = true;
+
             return newController.GetResult();
         }
     }
