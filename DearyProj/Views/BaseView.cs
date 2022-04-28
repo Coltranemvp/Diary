e1de86587c3bdb5bd974a60e50bb95a0e837cf95
Alexander <76205020+Coltranemvp@users.noreply.github.com>
2022-04-28 16:22:19 +0300

    Changed BaseView logic

--- DearyProj/Views/BaseView.cs
+++ DearyProj/Views/BaseView.cs
@@ -10,16 +10,61 @@ namespace DearyPetProj.Views
     public abstract class BaseView : IBaseView, IDisposable
     {
         private const string RepeatText = "Вы ввели пустое значение, повторите ввод!";
+        private bool _active;
+        private bool _disposedValue;
+
+        public event Action ChangeActiveState;
 
         public BaseView()
         {
             Console.Clear();
+
+            SubscribingEvents();           
         }
 
-        public virtual void Dispose()
+        public bool Active
         {
-
+            get => _active;
+            set
+            {
+                if (_active != value)
+                {
+                    _active = value;
+                    ChangeActiveState?.Invoke();
+                }
+            }
         }
+
+
+        public abstract bool ShowView();
+
+        public void Dispose() => Dispose(true);
+
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                    UnsubscribingEvents();
+
+                _disposedValue = true;
+            }
+        }
+
+
+        protected virtual void UnsubscribingEvents() 
+        {
+            //nothing here...
+        }
+
+
+        protected virtual void SubscribingEvents() 
+        {
+            //nothing here...
+        }
+
+
 
         public void ShowMessage(string messageText, ConsoleColor TextColor = ConsoleColor.White)
         {
@@ -46,6 +91,6 @@ namespace DearyPetProj.Views
             } while (isGetResponse);
 
             return response;
-        }        
+        }
     }
 }
--- DearyProj/Views/Interfaces/IBaseView.cs
+++ DearyProj/Views/Interfaces/IBaseView.cs
@@ -8,7 +8,12 @@ namespace DearyPetProj.Views.Interfaces
 {
     public interface IBaseView
     {
+        bool Active { get; set; }
+
         void ShowMessage(string messageText, ConsoleColor consoleColor);
+
         string GetUserResponse();
+
+        bool ShowView();
     }
 }
